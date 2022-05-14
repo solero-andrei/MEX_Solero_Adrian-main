cc448e3c4acbc773c65d891f924fe7529a039db0
solero-andrei <solerosolero29@gmail.com>
2022-05-14 17:57:10 +0800

    css

--- src/components/Items.js
+++ src/components/Items.js
@@ -3,12 +3,14 @@ import React from 'react'
 export default function TodoItem(props) {
     const { todo, removeTodo, completeTodo, importantTodo } = props
     return (
-        <div className={todo.completed ? "todo-row complete" : "todo-row"} style={todo.important ? { background: "red" } : { background: "lightgreen"}}>
-            {todo.text}
-            <div className="iconsContainer">
-                <button onClick={() => importantTodo(todo.id)}>Priority</button>
-                <button onClick={() => completeTodo(todo.id)}>Task Done</button>
-                <button onClick={() => removeTodo(todo.id)}>Delete Task</button>
+        <div className="todo-items">
+            <div className={todo.completed ? "todo-row complete" : "todo-row"} style={todo.important ? { background: "red" } : { }}>
+                <h1>{todo.text}</h1>
+                <div className="iconsContainer">
+                    <button className="prio" onClick={() => importantTodo(todo.id)}></button>
+                    <button className="finish" onClick={() => completeTodo(todo.id)}></button>
+                    <button className="remove" onClick={() => removeTodo(todo.id)}></button>
+                </div>
             </div>
         </div>
     )
